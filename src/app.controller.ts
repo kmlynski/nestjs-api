e4f89fef8f085dd79c2f6c09ba3502e36023aaa0
Kamil <kamil.mlynski1@gmail.com>
2023-11-23 11:02:54 +0100

    homework(day3): add delete method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
-@Controller()
+import { Controller, Get, Post, Body, Delete, Param } from '@nestjs/common';
+
+interface Category {
+  id: number;
+  name: string;
+}
+@Controller('categories')
 export class AppController {
+  private nextId = 8;
+  private categories: Category[] = [
+    { id: 1, name: 'Groceries' },
+    { id: 2, name: 'Cosmetics' },
+    { id: 3, name: 'Toys' },
+    { id: 4, name: 'Dairy' },
+    { id: 5, name: 'Fashion' },
+    { id: 6, name: 'Electronics' },
+    { id: 7, name: 'Games' },
+  ];
   @Get()
-  getHello(): string {
-    return 'Goodbye world';
+  getAll() {
+    return this.categories;
   }
 
-  @Get('/cat/:catId')
-  getCat() {
-    return { name: 'Mruczek', id: 100 };
+  @Get('/:id')
+  getCategoriesWithId(@Param('id') categoryId: number) {
+    this.categories.find((c) => c.id === categoryId);
+    return this.categories[categoryId];
+  }
+  @Post()
+  addNewCategory(@Body() payload: { name: string }) {
+    const category: Category = { id: this.nextId++, ...payload };
+    this.categories.push(category);
+    return category;
+  }
+
+  @Delete('/:id')
+  deleteCategoriesWithId(@Param('id') categoryId: string) {
+    const objectForDeletion = this.categories.find(
+      (c) => c.id === Number(categoryId),
+    );
+    this.categories.splice(this.categories.indexOf(objectForDeletion), 1);
+    return { message: 'Object deleted successfully ' };
   }
 }
