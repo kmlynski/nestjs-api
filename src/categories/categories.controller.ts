ca950bb39a312343cded3694ec84c7a33589fa40
Kamil <kamil.mlynski1@gmail.com>
2023-11-28 21:24:14 +0100

    homework(day6): add dependency injection to product service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,53 +1,28 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Delete,
-  Param,
-  NotFoundException,
-  ParseIntPipe,
-} from '@nestjs/common';
-import { NewCategoryDto } from './dtos/new-category.dto';
-import { Category } from './category.interface';
-import { categoriesList } from './categories-list';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { NewCategoryDto } from './dto/new-category.dto';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
 export class CategoriesController {
-  private nextId = 8;
+  constructor(private categoriesService: CategoriesService) {}
 
   @Get()
   getAll() {
-    return categoriesList;
-  }
-
-  @Get('/:id')
-  getSingleCategory(@Param('id', ParseIntPipe) categoryId: number) {
-    const category = this.findCategoryById(categoryId);
-    if (!category) {
-      throw new NotFoundException(`category with id: ${categoryId} not found`);
-    }
-    return category;
+    return this.categoriesService.getAll();
   }
 
   @Post()
   addNewCategory(@Body() payload: NewCategoryDto) {
-    const category: Category = { id: this.nextId++, ...payload };
-    categoriesList.push(category);
-    return category;
+    return this.categoriesService.addNew(payload);
   }
 
-  @Delete('/:id')
-  removeCategory(@Param('id', ParseIntPipe) categoryId: number) {
-    const objectForDeletion = this.findCategoryById(categoryId);
-    if (!objectForDeletion) {
-      throw new NotFoundException(`category with id: ${categoryId} not found`);
-    }
-    categoriesList.splice(categoriesList.indexOf(objectForDeletion), 1);
-    return { message: 'Object deleted successfully ' };
+  @Get(':id')
+  getSingleCategory(@Param('id') categoryId: number) {
+    return this.categoriesService.getOneById(categoryId);
   }
 
-  private findCategoryById(categoryId: number) {
-    return categoriesList.find((c) => c.id === categoryId);
+  @Delete(':id')
+  removeCategory(@Param('id') id: number) {
+    return this.categoriesService.removeById(id);
   }
 }
