0eeca8d719c681b5bb8aef05048c4cf590becc3b
Kamil <kamil.mlynski1@gmail.com>
2023-11-28 21:04:54 +0100

    homework(day): add products service

--- src/categories/category.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './categories.controller';
-import { ProductsController } from '../products/products.controller';
-@Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [],
-})
-export class AppModule {}
